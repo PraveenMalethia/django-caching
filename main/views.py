16edd42716d6789533440de9d6b9b936910e5c64
Parveen Kumar <prince.malethia.36@gmail.com>
2024-04-14 17:40:31 +0530

    Redis stack added and API improved

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
-
 # from django.views.decorators.cache import cache_page
 # from rest_framework import status
 from django.core.cache import cache
@@ -22,10 +21,10 @@ def GetAllPosts(request):
 
 @api_view(["GET"])
 def GetAllPostsCached(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
     cached_posts = cache.get("posts")
     if cached_posts is not None:
         return Response({"fetched_from_redis": cached_posts})
-    cache.set("posts", serializer.data, timeout=30)
-    return Response({"fetched_from_database": serializer.data})
+    posts = Post.objects.all()
+    json_posts = PostSerializer(posts, many=True).data
+    cache.set("posts", json_posts, timeout=15)
+    return Response({"fetched_from_database": json_posts})
